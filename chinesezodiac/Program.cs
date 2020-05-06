2851b3ba1a91b9fac5051595e1420cd870e1a403
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 19:08:43 +0900

    0506 05번 실습 문제

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,54 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            // 사용자에게 태어난 연도를 입력 받아 그 해의 띠를 출력하는 프로그램을 작성
+
+            Console.Write("태어난 연도를 입력하세요 >> ");
+            int year = int.Parse(Console.ReadLine());
+            int result = year % 12;
+
+            switch(result)
+            {
+                case 0:
+                    Console.WriteLine("원숭이");
+                    break;
+                case 1:
+                    Console.WriteLine("닭");
+                    break;
+                case 2:
+                    Console.WriteLine("개");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐");
+                    break;
+                case 5:
+                    Console.WriteLine("소");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼");
+                    break;
+                case 8:
+                    Console.WriteLine("용");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀");
+                    break;
+                case 10:
+                    Console.WriteLine("말");
+                    break;
+                case 11:
+                    Console.WriteLine("양");
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
         }
     }
 }
